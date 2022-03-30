16dbc33004b761198dc7530162882d43fea5d32b
Spoon God <spoon.is.god@gmail.com>
2022-03-30 09:28:39 -0400

    add colors and app.scss and global.scss

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,6 +2,14 @@ import "./topbar.scss"
 
 export default function Topbar() {
   return (
-    <div className='topbar'></div>
+    <div className='topbar'>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">dadaasd</a>
+        </div>
+        <h1>Hello</h1>
+        <div className="right"></div>
+      </div>
+    </div>
   )
 }
