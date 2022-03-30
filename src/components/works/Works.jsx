2bb72b1248171a07e61d8a5a1739335374090fb8
Spoon God <spoon.is.god@gmail.com>
2022-03-30 16:14:06 -0400

    mvp add mobile code and fix up so errors

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,7 +1,85 @@
-import "./works.scss"
+import { useState } from "react";
+import "./works.scss";
 
 export default function Works() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const data = [
+    {
+      id: "1",
+      icon: "",
+      title: "Web Design",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img:
+        "",
+    },
+    {
+      id: "2",
+      icon: "",
+      title: "Mobile Application",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "",
+    },
+    {
+      id: "3",
+      icon: "",
+      title: "MongoDB",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "",
+    },
+  ];
+
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+  
   return (
-    <div className="works" id="works"></div>
-  )
+    <div className="works" id="works">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.icon} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span>Projects</span>
+                </div>
+              </div>
+              <div className="right">
+                <img
+                  src=""
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <img
+        src=""
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src=""
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick()}
+      />
+    </div>
+  );
 }
