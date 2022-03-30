cd44c62be9a3b578980f20974f71185be089d72e
Spoon God <spoon.is.god@gmail.com>
2022-03-30 11:00:59 -0400

    added menu and top bar functions

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,14 +1,28 @@
 import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
 
-export default function Topbar() {
+export default function Topbar({menuOpen,setMenuOpen}) {
   return (
-    <div className='topbar'>
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro">dadaasd</a>
+          <a href="#intro" className="logo">.dev</a>
+          <div className="itemContainer">
+            <Person className="icon"/>
+            <span>(386)848-8399</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon"/>
+            <span>spoon.is.god@gmail.com</span>
+          </div>
         </div>
-        <h1>Hello</h1>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   )
